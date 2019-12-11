b83a7583e13896a980768f275d4c6b335d313d84
Leon <929509707@qq.com>
2019-12-11 18:13:22 +0800

    init mongodb

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
 from flask import Flask
+import mydb
 
 app = Flask(__name__)
+mongo = mydb.get_mongo(app)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    persons = mongo.db.test.find({'name': 'test'})
+    print(persons)
+    for person in persons:
+        print(person)
+    return 'found'
+
+
+@app.route('/person/<string:name>')
+def query_person(name):
+    persons = mongo.db.test.find({'name': name})
+    resp = ''.join([person['name'] for person in persons])
+    print(resp)
+    return resp
 
 
 if __name__ == '__main__':

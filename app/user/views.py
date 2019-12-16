6ac3ceb34843bcf4dc64b89d218ad13cec827b7d
Leon <929509707@qq.com>
2019-12-17 00:22:05 +0800

    get_user_info

--- app/user/views.py
+++ app/user/views.py
@@ -1,13 +1,17 @@
 import requests
-from flask import Blueprint, request, jsonify, session
+from flask import Blueprint, request, jsonify, session, current_app
 from app.db import get_db
+
 user = Blueprint('user', __name__)
 
 
 @user.route('/', methods=['GET'])
 def get_user_info():
-    appid, secret, code = request.data
-    url = 'https://api.weixin.qq.com/sns/jscode2session?appid=%s&secret=%s&js_code=%s&grant_type=authorization_code' % (appid, secret, code)
+    code = request.data
+    appid = current_app.config['APPID']
+    secret = current_app.config['SECRET']
+    url = 'https://api.weixin.qq.com/sns/jscode2session?appid=%s&secret=%s&js_code=%s&grant_type=authorization_code' \
+          % (appid, secret, code)
     r = requests.get(url)
     result = r.text
     return result
